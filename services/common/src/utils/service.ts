ffb471cf2e5059b9f6475d54d8f01c555710ba22
alex-mbolonzi <almbolonzi@gail.com>
2025-09-05 09:34:59 +0300

    fix: re-enable Node.js setup in CI and correct mongoSanitize middleware usage

--- services/common/src/utils/service.ts
+++ services/common/src/utils/service.ts
@@ -98,13 +98,15 @@ export default class Service {
       this.expressServer.use(Express.json());
       this.expressServer.use(_methodOverride());
       if (this.useMongo) {
-        mongoSanitize({
-          allowDots: true,
-          replaceWith: '_',
-          onSanitize: ({ req, key }: { req: Express.Request; key: string }) => {
-            console.warn(`request[${key}] has been sanitized`, req);
-          }
-        });
+        this.expressServer.use(
+          mongoSanitize({
+            allowDots: true,
+            replaceWith: '_',
+            onSanitize: ({ req, key }: { req: Express.Request; key: string }) => {
+              console.warn(`request[${key}] has been sanitized`, req);
+            }
+          })
+        );
       }
     }
 
